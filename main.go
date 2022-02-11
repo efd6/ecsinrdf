367659de32c725919eefff3775f7b0945b7ae696
Dan Kortschak <dan.kortschak@elastic.co>
2022-02-11 12:06:48 +1030

    make a small application for querying ecs and integrations

--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
 
 	"gonum.org/v1/gonum/graph/formats/rdf"
 	"gopkg.in/yaml.v3"
@@ -18,10 +22,13 @@ import (
 )
 
 func main() {
+	qry := flag.String("query", "", "specify a field path and type query path.to.field:type")
+	pkg := flag.String("pkg-path", ".", "specify the path to the root of the package(s) (ignored if query is not empty)")
 	root := flag.String("ecs-root", "", "specify the path to the root of the ecs repo")
 	version := flag.String("version", "", "specify the version of ECS to use (tag, branch or sha)")
 	flag.Parse()
-	if *root == "" || *version == "" {
+
+	if *root == "" || *version == "" || (*qry != "" && len(strings.Split(*qry, ":")) != 2) {
 		flag.Usage()
 		os.Exit(2)
 	}
@@ -52,24 +59,32 @@ func main() {
 		})
 	}
 
-	dec = yaml.NewDecoder(os.Stdin)
-	dec.KnownFields(true)
-	for {
-		var f []integration.Field
-		err := dec.Decode(&f)
+	if *qry == "" {
+		fr, err := fieldsReader(*pkg)
 		if err != nil {
-			if err == io.EOF {
-				break
-			}
 			log.Fatal(err)
 		}
-		integration.Statements("", f, func(s *rdf.Statement, err error) {
+		defer fr.Close()
+
+		dec = yaml.NewDecoder(fr)
+		dec.KnownFields(true)
+		for {
+			var f []integration.Field
+			err := dec.Decode(&f)
 			if err != nil {
-				log.Println(err)
-				return
+				if err == io.EOF {
+					break
+				}
+				log.Fatal(err)
 			}
-			statements = append(statements, s)
-		})
+			integration.Statements("", f, func(s *rdf.Statement, err error) {
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				statements = append(statements, s)
+			})
+		}
 	}
 
 	statements, err = rdf.URDNA2015(statements, statements)
@@ -82,23 +97,47 @@ func main() {
 		g.AddStatement(s)
 	}
 
-	// threat.enrichments.indicator.registry.path
-	fmt.Println(query.CandidateGraftsFor(g, "foo_package.indicator.registry.path", "keyword"))
-	fmt.Println(query.CandidateGraftsFor(g, "foo_package.registry.path", "keyword"))
-	fmt.Println(query.CandidateGraftsFor(g, "foo_package.registry.path", "text"))
-
-	fakeField("foo_package.indicator.registry.path", "keyword", func(s *rdf.Statement, err error) {
+	if *qry != "" {
+		parts := strings.Split(*qry, ":")
+		if len(parts) != 2 {
+			flag.Usage()
+			os.Exit(2)
+		}
+		cands, err := query.CandidateGraftsFor(g, strconv.Quote(parts[0]), strconv.Quote(parts[1]))
 		if err != nil {
+			fmt.Println(err)
 			return
 		}
-		g.AddStatement(s)
-	})
+		fmt.Println(cands)
+		return
+	}
 
-	fmt.Println(query.CandidateGraftsFor(g, "foo_package.indicator.registry.path", "keyword"))
-	fmt.Println(query.CandidateGraftsFor(g, "foo_package.registry.path", "keyword"))
-	fmt.Println(query.CandidateGraftsFor(g, "foo_package.registry.path", "text"))
-
-	fmt.Println(query.CandidateGraftsIn(g, "foo_package.indicator.registry.path"))
+	// Do some actual work.
+	notGroup := func(s *rdf.Statement) bool {
+		return s.Predicate.Value == "<as:type>" && s.Object.Value != `"group"`
+	}
+	p := query.PublishedFieldsIn(g)
+	p = p.Out(notGroup).In(notGroup).And(p)
+	for _, f := range p.Result() {
+		paths := g.Query(f).Out(func(s *rdf.Statement) bool {
+			return s.Predicate.Value == "<is:path>"
+		})
+		for _, n := range paths.Result() {
+			cands, err := query.CandidateGraftsIn(g, n.Value)
+			if len(cands) != 0 || err != nil {
+				fmt.Printf("%s\n", n.Value)
+			}
+			if err != nil {
+				fmt.Printf("\t%s: %v\n", n.Value, err)
+			}
+			for _, c := range cands {
+				fmt.Printf("\t%s\n", c)
+			}
+			if len(cands) != 0 || err != nil {
+				fmt.Println()
+			}
+		}
+	}
 }
 
 const nestedPath = "generated/ecs/ecs_nested.yml"
@@ -113,4 +152,54 @@ func ecsSpec(path, version string) (io.Reader, error) {
 		return nil, err
 	}
 	return &buf, nil
+}
+
+func fieldsReader(path string) (io.ReadCloser, error) {
+	var (
+		readers []io.Reader
+		mr      multiReaderCloser
+	)
+
+	err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+		if err != nil || d.IsDir() {
+			return nil
+		}
+		if filepath.Ext(path) != ".yml" {
+			return nil
+		}
+		if filepath.Base(filepath.Dir(path)) != "fields" {
+			return nil
+		}
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		mr.files = append(mr.files, f)
+		readers = append(readers, f)
+		return nil
+	})
+	if err != nil {
+		mr.Close()
+		return nil, err
+	}
+
+	mr.Reader = io.MultiReader(readers...)
+
+	return &mr, nil
+}
+
+type multiReaderCloser struct {
+	io.Reader
+	files []*os.File
+}
+
+func (mr *multiReaderCloser) Close() error {
+	var err error
+	for _, f := range mr.files {
+		e := f.Close()
+		if err != nil {
+			err = e
+		}
+	}
+	return err
 }
--- query/grafting.go
+++ query/grafting.go
@@ -10,31 +10,52 @@ import (
 	"gonum.org/v1/gonum/graph/formats/rdf"
 )
 
+// PublishedFieldsIn returns a query holding published fields in the graph.
+func PublishedFieldsIn(g *rdf.Graph) rdf.Query {
+	// Selecting the true node is redundant with the
+	// isPublished helper, but reduces the search space.
+	node, ok := g.TermFor(`"true"`)
+	if !ok {
+		return rdf.Query{}
+	}
+	return g.Query(node).In(isPublished).Unique()
+}
+
 // CandidateGrafts returns a list of potential ECS graft candidate
 // destinations for the field with the provided full path. The field
 // must already be in the the graph. Candidates will have the same type
 // as the query field and will have matching path suffixes.
 //
+// The full path is expected to be quoted as an unqualified RDF literal.
+//
 // The graph g is expected to be an ECS graph with statements relating
 // to the ECS and package field constructed by the schema and integration
 // packages in this repo.
 func CandidateGraftsIn(g *rdf.Graph, full string) ([]string, error) {
-	path := strings.Split(full, ".")
-	node, ok := g.TermFor(strconv.Quote(full))
+	node, ok := g.TermFor(full)
 	if !ok {
 		return nil, errors.New("not found")
 	}
+	full, err := strconv.Unquote(full)
+	if err != nil {
+		return nil, err
+	}
+	path := strings.Split(full, ".")
 
 	// Select nodes that that are the right full path.
 	q := g.Query(node).In(byPath)
 	// Confirm it is published and get its type. There should be exactly one.
-	typs := q.And(q.Out(isPublished).In(isPublished)).Out(byUsedType).Unique().Result()
+	typs := q.Out(isPublished).In(isPublished).And(q).Out(byUsedType).Unique().Result()
 	switch len(typs) {
 	case 0:
 		return nil, errors.New("no type")
 	case 1:
 	default:
-		return nil, fmt.Errorf("found multiple types: %v", typs)
+		typeNames := make([]string, len(typs))
+		for i, s := range typs {
+			typeNames[i] = s.Value
+		}
+		return nil, fmt.Errorf("found multiple types: %v", typeNames)
 	}
 
 	// Get all the other nodes with the same name.
@@ -50,23 +71,28 @@ func CandidateGraftsIn(g *rdf.Graph, full string) ([]string, error) {
 // Candidates will have the same type as the query field and will have
 // matching path suffixes.
 //
+// The full path and typ are expected to be quoted as unqualified RDF literals.
+//
 // The graph g is expected to be an ECS graph with statements relating
 // to the ECS field constructed by the schema packages in this repo.
 // It may contain statements relating to integration fields.
 func CandidateGraftsFor(g *rdf.Graph, full, typ string) ([]string, error) {
+	full, err := strconv.Unquote(full)
+	if err != nil {
+		return nil, err
+	}
 	path := strings.Split(full, ".")
 	node, ok := g.TermFor(strconv.Quote(path[len(path)-1]))
 	if !ok {
 		return nil, errors.New("path not found")
 	}
-	quotedTyp := strconv.Quote(typ)
 
 	// Select nodes that that are the right name.
 	q := g.Query(node).In(byName)
 	// Get the typ node.
-	typs, ok := g.TermFor(quotedTyp)
+	typs, ok := g.TermFor(typ)
 	if !ok {
-		return nil, errors.New("typ not found")
+		return nil, errors.New("type not found")
 	}
 
 	// Walk the path.
@@ -79,17 +105,18 @@ func walkMatchingPath(q rdf.Query, typ rdf.Term, path []string) []string {
 	matchingType := func(s *rdf.Statement) bool {
 		return s.Predicate.Value == "<is:type>" && s.Object.Value == typ.Value
 	}
-	q = q.And(q.Out(matchingType).In(matchingType))
+	q = q.Out(matchingType).In(matchingType).And(q)
 
 	// Walk the path.
 	var final []rdf.Term
 	for i := len(path) - 2; i >= 0; i-- {
-		quotedName := strconv.Quote(path[i])
-
 		c := q.In(hasChild)
-		q = c.Out(func(s *rdf.Statement) bool {
+
+		quotedName := strconv.Quote(path[i])
+		matchingName := func(s *rdf.Statement) bool {
 			return s.Predicate.Value == "<is:name>" && s.Object.Value == quotedName
-		}).In(byName).And(c)
+		}
+		q = c.Out(matchingName).In(matchingName).And(c)
 
 		r := q.Out(byPath).Unique().Result()
 		if len(r) == 0 {
